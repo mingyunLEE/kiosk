222fbbb35f28d69204af79b9faed548c65a41b65
mingyunLEE <leemingyun@naver.com>
2023-04-28 05:40:35 +0900

    Fix MemberController code

--- src/main/java/developingman/kiosk/controller/MemberController.java
+++ src/main/java/developingman/kiosk/controller/MemberController.java
@@ -1,4 +1,9 @@
 package developingman.kiosk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // (1)
+@RequestMapping("/v1/members") //(2)
 public class MemberController {
 }
