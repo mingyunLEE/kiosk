0d826c15e5c76bf09f874e2a19c3af5c0d9bd004
mingyunLEE <leemingyun@naver.com>
2023-04-28 05:41:26 +0900

    Fix SoupController class

--- src/main/java/developingman/kiosk/controller/SoupController.java
+++ src/main/java/developingman/kiosk/controller/SoupController.java
@@ -1,4 +1,9 @@
 package developingman.kiosk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/soups")
 public class SoupController {
 }
