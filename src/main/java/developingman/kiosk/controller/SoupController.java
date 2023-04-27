380e63759db771aafddc29bfd168a2fd2396ea22
mingyunLEE <leemingyun@naver.com>
2023-04-28 07:22:32 +0900

    Fix SoupController class
    Add Handler method

--- src/main/java/developingman/kiosk/controller/SoupController.java
+++ src/main/java/developingman/kiosk/controller/SoupController.java
@@ -1,9 +1,32 @@
 package developingman.kiosk.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/soups")
+@RequestMapping(value = "/v1/soups", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class SoupController {
+    @PostMapping
+    public String postSoup(@RequestParam("name") String name,
+                           @RequestParam("price") String price){
+        String response =
+                "{\"" +
+                        "\"name\":\""+name+"\",\"" +
+                        "phone\":\"" + price+
+                        "\"}";
+        return response;
+    }
+    @GetMapping("/{soup-id")
+    public String getSoup(@PathVariable("soup-id")long soupId){
+        System.out.println("# SoupId: " + soupId);
+
+        return null;
+    }
+
+    @GetMapping
+    public String getSoups(){
+        System.out.println("# get Soups");
+
+        return null;
+    }
 }
